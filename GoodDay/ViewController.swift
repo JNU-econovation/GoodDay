2b61b548795799490ca9dbbe799429dc907dd3f2
oripk <fine6115@gmail.com>
2021-12-30 15:53:27 +0900

    inital project

---
+++ GoodDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodDay
+//
+//  Created by LIMCHEASUENG on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
